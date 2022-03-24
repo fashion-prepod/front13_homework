dd8403e92a78830bdae9a8d7029954360daa2d12
Sasha867 <sashacordoba@mail.ru>
2022-03-24 15:12:39 +0300

    All proect

---
+++ index.js
@@ -0,0 +1,44 @@
+
+
+
+// let sum = 0;
+
+// for (let a = 0; ; a++) {
+//   let num = prompt("enter numbers");
+//   if (num !== null) {
+//     num = +num;
+//     sum = sum + num;
+//   } else {
+//     alert("result: " + sum / a);
+//     break;
+//   }
+// }
+
+
+// for (let a = 0; ; a++) {
+//   let num = prompt("enter numbers");
+//   if (isNaN(num)) {
+//     alert("wrong value");
+//   } else if (num === null) {
+//     alert("result " + sum / a);
+//     break;
+//   }
+//   num = +num;
+//   sum = sum + num;
+// }
+
+// let sum = 0;
+
+// for (a = 0; ; a++) {
+//   let num = prompt("enter your numbers");
+//   if (isNaN(num)) {
+//     alert("wrong value");
+//   } else if (num === null) {
+//     num = +num;
+//     sum += num;
+//     alert("result: " + sum / a);
+//   } else {
+//     alert("enter your numbers");
+//     break;
+//   }
+// }
