bea4ee9f38ebb6767d8ad7aa6bdcd84a97918cd2
Sasha867 <sashacordoba@mail.ru>
2022-03-25 15:02:44 +0300

    Task_8

--- index.js
+++ index.js
@@ -1,32 +1,3 @@
-
-
-
-// let sum = 0;
-
-// for (let a = 0; ; a++) {
-//   let num = prompt("enter numbers");
-//   if (num !== null) {
-//     num = +num;
-//     sum = sum + num;
-//   } else {
-//     alert("result: " + sum / a);
-//     break;
-//   }
-// }
-
-
-// for (let a = 0; ; a++) {
-//   let num = prompt("enter numbers");
-//   if (isNaN(num)) {
-//     alert("wrong value");
-//   } else if (num === null) {
-//     alert("result " + sum / a);
-//     break;
-//   }
-//   num = +num;
-//   sum = sum + num;
-// }
-
 //Task_4
 // for (i = 0; i <= 0; i++) {
 // let sum = 0;
@@ -48,19 +19,19 @@
 
 //Task_1
 
-for (a = 0; a < Infinity; a++) {
-  let enterNumber = prompt("enter your number N");
-  if (!isNaN(enterNumber) && enterNumber !== null && +enterNumber >= 1) {
-    enterNumber = +enterNumber;
-    let sum = 0;
-    for (let i = 1; i <= enterNumber; i++) {
-      sum += i;
-    }
-    alert(`you entered a number ${enterNumber} sum is: ${sum}`);
-  } else {
-    alert("wrong value enter your number");
-  }
-}
+// for (a = 0; a < Infinity; a++) {
+//   let enterNumber = prompt("enter your number N");
+//   if (!isNaN(enterNumber) && enterNumber !== null && +enterNumber >= 1) {
+//     enterNumber = +enterNumber;
+//     let sum = 0;
+//     for (let i = 1; i <= enterNumber; i++) {
+//       sum += i;
+//     }
+//     alert(`you entered a number ${enterNumber} sum is: ${sum}`);
+//   } else {
+//     alert("wrong value enter your number");
+//   }
+// }
 
 //Task_3;
 // for (a = 10; a < 100; a++) {
@@ -81,3 +52,55 @@ for (a = 0; a < Infinity; a++) {
 // for(i=1;i<=numberEnteredUser;){
 
 // }
+
+//Task_5
+
+// for (i = 0; ; i++) {
+//   let userNumber = prompt("enter your first number");
+//   let userNumber_2 = prompt("enter your second number");
+//   let multiplication = prompt("enter multiplication your numbers");
+//   if (
+//     isNaN(userNumber) ||
+//     isNaN(userNumber_2) ||
+//     userNumber === "" ||
+//     userNumber_2 === "" ||
+//     userNumber === null ||
+//     userNumber_2 === null
+//   ) {
+//     alert("enter your numbers");
+//     break;
+//   }
+//   userNumber = +userNumber;
+//   userNumber_2 = +userNumber_2;
+//   multiplication = +multiplication;
+//   if (multiplication !== userNumber * userNumber_2) {
+//     multiplication = userNumber * userNumber_2;
+
+//     alert(`result: ${multiplication}`);
+//   } else {
+//     alert(`Good`);
+//   }
+// }
+
+//Task_7
+for (a = 0; ; a++) {
+  let userNumberOne = prompt("enter your number N");
+  let userNumberTwo = prompt("enter your number M");
+  if (
+    isNaN(userNumberOne) ||
+    isNaN(userNumberTwo) ||
+    userNumberOne === "" ||
+    userNumberTwo === "" ||
+    userNumberOne === null ||
+    userNumberTwo === null
+  ) {
+    alert("enter your numbers");
+    continue;
+  }
+  userNumberOne = +userNumberOne;
+  userNumberTwo = +userNumberTwo;
+  for (let i = userNumberOne; i <= userNumberTwo; i++) {
+    let multiplication = `${i} * ${i}`;
+    alert(multiplication);
+  }
+}
