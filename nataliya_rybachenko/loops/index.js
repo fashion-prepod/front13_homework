e359340b6f43d08b39bdececc3f896f20b93bbb0
Rybachenka <7381458@mail.ru>
2022-04-01 01:22:57 +0300

    added exercise 1

--- nataliya_rybachenko/loops/index.js
+++ nataliya_rybachenko/loops/index.js
@@ -1,1 +1,14 @@
-console.log('Hello world!!!');
+// 1. Написать программу, которая вычисляет сумму чисел от 1 до n. Значение n вводится с клавиатуры.
+// Проверить число n на корректность (чтобы это было число, чтобы оно не было меньше 1, чтобы это было не null).
+
+// let numberN=prompt('Enter a number');
+// if(isNaN(numberN) || numberN<1){
+//     console.log('Enter the correct number')
+// }else{
+//     numberN=+numberN;
+//     let sumNumber=0;
+//     for(let i=1; i<=numberN; i++){
+//     sumNumber+=i;
+// }
+// console.log(sumNumber);
+// }
