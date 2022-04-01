60a8e4b24475d4ab2890ea4fa7402620d14db222
Xaky <81919274+Xaky0105@users.noreply.github.com>
2022-04-01 23:09:04 +0300

    arrays part 3, solved task 1

--- anton_starosotnikov/arrays/index.js
+++ anton_starosotnikov/arrays/index.js
@@ -1,8 +1,19 @@
 //Task 1, 2, 3 - решили в аудитории
 //Task 4 Сформировать массив из чисел, которые делятся на 3 (Количество элементов массива: 8)
 
-const arr = [];
-for (let i = 1; arr.length !== 8; i++) {
-    arr[arr.length] = i*3;
-}
-console.log(arr);
+// const arr = [];
+// for (let i = 1; arr.length !== 8; i++) {
+//     arr[arr.length] = i*3;
+// }
+// console.log(arr);
+
+//Part 3 arrays
+
+//Task 1  С помощью цикла создать массив, каждый 
+// элемент которого равен квадрату своего номера.
+
+// const arr = [];
+// for (let i = 0; arr.length !== 10; i++) {
+//     arr[i] = i * i;
+// }
+// console.log(arr);
