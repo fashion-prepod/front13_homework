9fc0ba3fa5b28754f4a305b84a1bfe07643dabb8
Sasha867 <sashacordoba@mail.ru>
2022-04-01 13:14:42 +0300

    HW_1 Task_1

---
+++ loopsHomeWork/index.js
@@ -0,0 +1,18 @@
+// //Task_1
+// Написать программу, которая вычисляет сумму чисел от 1 до n. Значение n вводится с клавиатуры.
+// Проверить число n на корректность (чтобы это было число, чтобы оно не было меньше 1, чтобы это было не null).
+
+// for (;;) {
+//   let enterNumber = prompt("enter your number N");
+//   if (!isNaN(enterNumber) && enterNumber !== null && +enterNumber >= 1) {
+//     enterNumber = +enterNumber;
+//     let sum = 0;
+//     for (let i = 1; i <= enterNumber; i++) {
+//       sum += i;
+//     }
+//     alert(`you entered a number ${enterNumber} sum is: ${sum}`);
+//   } else {
+//     alert("wrong value enter your number");
+//   }
+// }
+
