ac188d9aa4eb97828f804fa416cc75d0eb76d494
Xaky <81919274+Xaky0105@users.noreply.github.com>
2022-04-01 23:48:23 +0300

    arrays part 3, solved task 2

--- anton_starosotnikov/arrays/index.js
+++ anton_starosotnikov/arrays/index.js
@@ -17,3 +17,17 @@
 //     arr[i] = i * i;
 // }
 // console.log(arr);
+
+//Task 2 С помощью цикла создать массив, 
+// который одинаково читается как слева направо, 
+// так и справа налево (Количество элементов массива: 9).
+
+// const arr = [];
+// for (let i = 0; arr.length !== 9; ) {
+//     if (arr.length < 5) {
+//         arr[arr.length] = ++i;
+//     } else {
+//         arr[arr.length] = --i;
+//     }
+// }
+// console.log(arr)
