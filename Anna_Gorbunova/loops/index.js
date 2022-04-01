81df9700762b2b34515b0db2e72a2f76997abecd
GorbunovaAnna <anyutachke@gmail.com>
2022-04-01 15:23:30 +0300

    added task 4

--- Anna_Gorbunova/loops/index.js
+++ Anna_Gorbunova/loops/index.js
@@ -1,1 +1,25 @@
-console.log('hello');
+// 4
+// Написать программу, которая выводит среднее
+// арифметическое n чисел введенных пользователем.
+// Ввод чисел прекращается
+// тогда, когда нажали «отмена», после чего выводится
+// среднее арифметическое.
+
+
+// let sum = 0;
+
+// for(let i = 0; ; i++) {
+    
+//     let userNumber = prompt('Enter number', '');
+
+//     if(userNumber !== null) {
+
+//         userNumber = Number(userNumber);
+//         sum += userNumber;
+//     } else {
+//         let result = sum / i;
+//         alert(`Result is ${result}`);
+//         break;
+//     }
+
+// }
