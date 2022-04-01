794effefc4dcc009d5a8e4d661019796932e72a7
GorbunovaAnna <anyutachke@gmail.com>
2022-04-01 15:45:49 +0300

    added task 5

--- Anna_Gorbunova/loops/index.js
+++ Anna_Gorbunova/loops/index.js
@@ -23,3 +23,37 @@
 //     }
 
 // }
+
+
+// 5
+// Проверка на таблицу умножения
+// Пользователь вводит два случайных числа
+// После чего вводит еще одно число, которое должно являться произведением 
+// первых двух
+// Если пользователь ввел правильное произведение вывести собщение что он молодец!
+// Если нет, выводим пользователю правильный ответ.
+
+// for(; ;) {
+
+//     let firstMultiplier = prompt('Enter first multiplier', '');
+//     let secondMultiplier = prompt('Enter second multiplier', '');
+//     let result = prompt('Enter result', '');
+
+//     if(firstMultiplier === null || secondMultiplier === null || result === null) {
+//         alert('You do not enter number');
+//         break;
+//     } 
+
+//     if(firstMultiplier === '' || secondMultiplier === '' || result === '') {
+//         alert('You do not enter nothing');
+//     } else if (isNaN(firstMultiplier) || isNaN(secondMultiplier) || isNaN(result)) {
+//         alert('Enter number!');
+//     } else {
+//         let sum = firstMultiplier * secondMultiplier;
+//         if(sum === +result) {
+//             alert('You are right!');
+//         } else {
+//             alert(`Right result ${sum}`);
+//         }
+//     }
+// }
