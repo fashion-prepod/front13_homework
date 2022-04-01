a2980beacf37ae1af734712dcd36fc6cb4b7b9ab
Sasha867 <sashacordoba@mail.ru>
2022-04-01 13:25:05 +0300

    HW_1 Task_5

--- loopsHomeWork/index.js
+++ loopsHomeWork/index.js
@@ -76,3 +76,39 @@
 //     break;
 //   }
 // }
+
+//Task_5
+// Проверка на таблицу умножения
+// Пользователь вводит два случайных числа
+// После чего вводит еще одно число, которое должно являться произведением
+// первых двух
+// Если пользователь ввел правильное произведение вывести собщение что он молодец!
+// Если нет, выводим пользователю правильный ответ.
+
+// for (let i = 0; ; i++) {
+//   let userNumber = prompt("enter your first number");
+//   let userNumber_2 = prompt("enter your second number");
+//   let multiplication = prompt("enter multiplication your numbers");
+//   if (
+//     isNaN(userNumber) ||
+//     isNaN(userNumber_2) ||
+//     userNumber === "" ||
+//     userNumber_2 === "" ||
+//     userNumber === null ||
+//     userNumber_2 === null
+//   ) {
+//     alert("enter your numbers");
+//     break;
+//   }
+//   userNumber = +userNumber;
+//   userNumber_2 = +userNumber_2;
+//   multiplication = +multiplication;
+//   if (multiplication !== userNumber * userNumber_2) {
+//     multiplication = userNumber * userNumber_2;
+
+//     alert(`result: ${multiplication}`);
+//   } else {
+//     alert(`Good`);
+//   }
+// }
+
