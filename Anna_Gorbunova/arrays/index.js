43cfa878d2c682529a8c1625c0716c76089add24
GorbunovaAnna <anyutachke@gmail.com>
2022-04-03 14:34:27 +0300

    added task 4

---
+++ Anna_Gorbunova/arrays/index.js
@@ -0,0 +1,14 @@
+// 4
+// Сформировать массив из чисел, которые делятся на 3.
+// Количество элементов массива: 8.
+
+// const arrayNumbers = [];
+// let position = 0;
+
+// for (let i = 1; arrayNumbers.length < 8; i++) {
+//   if (i % 3 === 0) {
+//     arrayNumbers[position] = i;
+//     position++;
+//   }
+// }
+// console.log(arrayNumbers);
