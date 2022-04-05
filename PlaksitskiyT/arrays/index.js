f252ea6a406c35f62f299143ad791d99e4b24ddd
Timofei Plaksitski <energye@tut.by>
2022-04-05 20:26:10 +0300

    Added next task number 4 *

--- PlaksitskiyT/arrays/index.js
+++ PlaksitskiyT/arrays/index.js
@@ -20,3 +20,11 @@
 // }
 // console.log(arr);
 
+// Сформировать массив из чисел, которые делятся на 3 (Количество элементов массива: 8)
+
+// const arr = [];
+// for (let i = 1; arr.length !== 8; i++){
+//   arr[arr.length] = i*3;
+//   }
+//   console.log(arr);
+
