8f6878548bcbf57e173b2bd47e1e5d3b2a67d93f
Rybachenka <7381458@mail.ru>
2022-04-06 22:25:12 +0300

    added task 4

--- nataliya_rybachenko/arrays/index.js
+++ nataliya_rybachenko/arrays/index.js
@@ -22,8 +22,24 @@
 // 3.С помощью цикла создать массив, каждый элемент
 // которого равен квадрату своего номера.
 
+// const arr = [];
+// for (let i = 0; arr.length < 8; i++) {
+//   arr[arr.length] = i ** 2;
+// }
+// console.log(arr);
+
+// 4.С помощью цикла создать массив, который
+// одинаково читается как слева направо, так и справа
+// налево (Количество элементов массива: 9)
+
 const arr = [];
-for (let i = 0; arr.length < 8; i++) {
-  arr[arr.length] = i ** 2;
+for (let i = 0; ; ) {
+  if (arr.length < 4) {
+    arr[arr.length] = i++;
+  } else if (arr.length >= 4 && arr.length < 9) {
+    arr[arr.length] = i--;
+  } else {
+    break;
+  }
 }
 console.log(arr);
