ff9ea04928228562e2f4de29de8ff9aabe671bf0
Rybachenka <7381458@mail.ru>
2022-04-06 22:18:17 +0300

    added task 2

--- nataliya_rybachenko/arrays/index.js
+++ nataliya_rybachenko/arrays/index.js
@@ -1,9 +1,20 @@
 // 1. Создать массив из 10ти элементов и вывести на
 // экран сумму всех элементов массива.
 
-const arr = [1, 2, 2, 3, 3, 5, 6, 8, 4, 7];
-let sum = 0;
-for (let i = 0; i < arr.length; i++) {
-  sum += arr[i];
+// const arr = [1, 2, 2, 3, 3, 5, 6, 8, 4, 7];
+// let sum = 0;
+// for (let i = 0; i < arr.length; i++) {
+//   sum += arr[i];
+// }
+// console.log(sum);
+
+// 2. Сформировать массив из чисел, которые делятся на
+// 3 (Количество элементов массива: 8)
+
+const arr = [];
+for (let i = 0; arr.length < 8; i++) {
+  if (i % 3 === 0) {
+    arr[arr.length] = i;
+  }
 }
-console.log(sum);
+console.log(arr);
