ff7434553a2a1ab47a93cfa5069fd4e724c90d0a
Archi_Boom <artur.patyomkin@mail.ru>
2022-04-07 20:43:42 +0300

    Task_3

--- Artur_Patyomkin/loops/index.js
+++ Artur_Patyomkin/loops/index.js
@@ -25,7 +25,7 @@
 // }
 
 
-// 2. Пользователь вводит свой возраст. Вывести в консоль 
+// 2. Пользователь вводит свой возраст. Вывести в консоль
 // сколько лет юудет пользователю каждый год до 2030
 // например вводим 14:
 // выводим:
@@ -34,19 +34,41 @@
 // и тд
 // в 2030 будет 22
 
-const finalYear = 2030;
-let currentYear = 2022;
-let userAge = prompt('please, enter your age:');
+// const finalYear = 2030;
+// let currentYear = 2022;
+// let userAge = prompt('please, enter your age:');
 
-if (userAge === null) {
-   console.log('you are cancelled the game')
-} else {
-   userAge = Number(userAge);
-   if (isNaN(userAge)) {
-      console.log('Please enter only numbers!!!');
+// if (userAge === null) {
+//    console.log('you are cancelled the game')
+// } else {
+//    userAge = Number(userAge);
+//    if (isNaN(userAge)) {
+//       console.log('Please enter only numbers!!!');
+//    } else {
+//       for (; currentYear < finalYear; currentYear++, userAge++) {
+//          console.log(`in ${currentYear + 1} he will be ${userAge + 1}`);
+//       }
+//    }
+// }
+
+// 3. Пользователя спрашивают пароль
+// если пароль введен неправильно, спрашивать еще раз
+// если правильно вывести "добро пожаловать"
+// если отмена вывести "вход отменен"
+
+const password = '123check';
+let infinitLoop = true;
+while (infinitLoop) {
+   let userPassword = prompt('enter your password');
+   if (userPassword === null) {
+      console.log('entering is cancelled');
+      infinitLoop = false;
    } else {
-      for (; currentYear < finalYear; currentYear++, userAge++) {
-         console.log(`in ${currentYear + 1} he will be ${userAge + 1}`);
+      if (password === userPassword) {
+         console.log('you are wellcome!!!');
+         break;                               //проверочка другого способа выйти из цикла
+      } else {
+         console.log('wrong password! try again');
       }
    }
 }
