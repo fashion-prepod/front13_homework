23d1f3b041720c81be8d0928bb25eb1bb72bbf51
Sasha867 <sashacordoba@mail.ru>
2022-04-08 00:14:42 +0300

    Task_2.1

--- sasha_lozichny/array methods/index.js
+++ sasha_lozichny/array methods/index.js
@@ -1,9 +1,22 @@
 // Task_1. Дана строка "Мы любим javascript"
 // изменить эту строку на "Мы ОБОЖАЕМ javascript"
 
-let string = "Мы любим javascript";
-let arr = string.split(" ");
-arr.splice(1, 1, "ОБОЖАЕМ");
-let stringChanged = arr.join(' ');
-console.log(stringChanged);
+// let string = "Мы любим javascript";
+// let arr = string.split(" ");
+// arr.splice(1, 1, "ОБОЖАЕМ");
+// let stringChanged = arr.join(' ');
+// console.log(stringChanged);
 
+
+// Task_2.1 Даны два массива
+// 1. Пользователь вводит имя, если это имя есть в массиве1 добавьте его копию в массив2
+
+let people1 = ['Samuel', 'Jack', 'Thomas', 'Henry', 'Leo', 'Connor', 'David', 'Ryan'];
+let people2 = ['Connor', 'Stanley', 'Leo', 'Albert', 'Owen', 'Oliver', 'Ethan', 'Thomas'];
+let userName = prompt("enter your name");
+for (let i = 0; i < people1.length; i++) {
+  if (userName === people1[i]) {
+    people2.push(userName);
+  }
+}
+  console.log(people2);
