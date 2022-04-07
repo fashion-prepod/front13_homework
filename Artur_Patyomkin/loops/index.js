2f254098d917bda35455b9806623090e974a6e98
Archi_Boom <artur.patyomkin@mail.ru>
2022-04-07 20:04:55 +0300

    Task_2

--- Artur_Patyomkin/loops/index.js
+++ Artur_Patyomkin/loops/index.js
@@ -1,25 +1,52 @@
 // 1. Пользователь вводит год своего рождения
 // вывести в консоль инфу сколько пользователю было в разные года до текущего
-// например: 
+// например:
 // вводим 2003:
 // выводим: в 2003 было 1 год
 // в 2004 было 2 год
-// в 2005 было 3 год 
+// в 2005 было 3 год
 // и тд
 // в 2022 было 19 год
 
 
-let userBirthday = prompt('enter a year of your birthday :');
-if (userBirthday === null) {
+// let userBirthday = prompt('enter a year of your birthday :');
+// if (userBirthday === null) {
+//    console.log('you are cancelled the game')
+// } else {
+//    userBirthday = Number(userBirthday);
+//    if (isNaN(userBirthday)) {
+//       console.log('Please enter only numbers!!!');
+//    } else {
+//       let howManyYears = 2022 - userBirthday;
+//       for (let i = 0; i <= howManyYears; i++, userBirthday++) {
+//          console.log(`in ${userBirthday} he was ${i} year`);
+//       }
+//    }
+// }
+
+
+// 2. Пользователь вводит свой возраст. Вывести в консоль 
+// сколько лет юудет пользователю каждый год до 2030
+// например вводим 14:
+// выводим:
+// в 2023 будет 15
+// в 2024 будет 16
+// и тд
+// в 2030 будет 22
+
+const finalYear = 2030;
+let currentYear = 2022;
+let userAge = prompt('please, enter your age:');
+
+if (userAge === null) {
    console.log('you are cancelled the game')
 } else {
-   userBirthday = Number(userBirthday);
-   if (isNaN(userBirthday)) {
+   userAge = Number(userAge);
+   if (isNaN(userAge)) {
       console.log('Please enter only numbers!!!');
    } else {
-      let howManyYears = 2022 - userBirthday;
-      for (let i = 0; i <= howManyYears; i++, userBirthday++) {
-         console.log(`in ${userBirthday} he was ${i} year`);
+      for (; currentYear < finalYear; currentYear++, userAge++) {
+         console.log(`in ${currentYear + 1} he will be ${userAge + 1}`);
       }
    }
 }
