d5f937fbf409d980dae6ad051bc27710042ee80d
Archi_Boom <artur.patyomkin@mail.ru>
2022-04-08 15:56:53 +0300

    task_4.2.1

--- Artur_Patyomkin/arrays/index.js
+++ Artur_Patyomkin/arrays/index.js
@@ -68,7 +68,26 @@
 // 4.1 Дана строка "Мы любим javascript"
 // изменить эту строку на "Мы ОБОЖАЕМ javascript"
 
-let userString = "Мы любим Javascript";
-let arr = userString.split(' ');
-arr.splice(1, 1, "ОБОЖАЕМ");
-console.log(arr);
+// let userString = "Мы любим Javascript";
+// let arr = userString.split(' ');
+// arr.splice(1, 1, "ОБОЖАЕМ");
+// console.log(arr);
+
+
+// 4.2 Даны два массива
+// 1. Пользователь вводит имя, если это имя есть в массиве1 добавьте его копию в массив2
+// 2. Пользователь вводит имя, если это имя есть в массиве2 удалите это имя из массива
+// 3. Создайте новый массив только из тех имен, которые есть в обоих массивах
+// 4. Объедините два массива так, чтобы в получившемся массиве не было одинаковых имен
+
+let people1 = ['Samuel', 'Jack', 'Thomas', 'Henry', 'Leo', 'Connor', 'David', 'Ryan'];
+let people2 = ['Connor', 'Stanley', 'Leo', 'Albert', 'Owen', 'Oliver', 'Ethan', 'Thomas'];
+let userName = prompt('enter name');
+
+for (let i = 0; i < people1.length - 1; i++) {
+   if (userName === people1[i]) {
+      people2.push(people1[i]);
+   }
+}
+console.log(people1);
+console.log(people2);
