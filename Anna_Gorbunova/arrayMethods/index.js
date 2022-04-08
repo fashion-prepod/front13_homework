c65c7c418503c24e8c0e40cf892370bc5f874b16
GorbunovaAnna <anyutachke@gmail.com>
2022-04-08 12:09:22 +0300

    added task 2

--- Anna_Gorbunova/arrayMethods/index.js
+++ Anna_Gorbunova/arrayMethods/index.js
@@ -12,3 +12,16 @@
 //     }
 // }
 // console.log(people2);
+
+
+// 2. 
+// Пользователь вводит имя, если это имя есть в массиве2 удалите это имя из массива
+
+// let people1 = ['Samuel', 'Jack', 'Thomas', 'Henry', 'Leo', 'Connor', 'David', 'Ryan'];
+// let people2 = ['Connor', 'Stanley', 'Leo', 'Albert', 'Owen', 'Oliver', 'Ethan', 'Thomas'];
+
+// const userName = prompt('Enter name', '');
+
+// let indexName = people2.indexOf(userName);
+// people2.splice(indexName, 1);
+// console.log(people2);
