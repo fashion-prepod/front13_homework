3ec8f705dcab45e0870d854d32e924a8e422cdba
Timofei Plaksitski <energye@tut.by>
2022-04-08 14:03:02 +0300

    HW task 2

--- PlaksitskiyT/arrays_methods/index.js
+++ PlaksitskiyT/arrays_methods/index.js
@@ -1,11 +1,11 @@
 // 1. Дана строка "Мы любим javascript"
 // изменить эту строку на "Мы ОБОЖАЕМ javascript"
 
-let str = "Мы любим javascript";
-const strNew = str.split(' ');
-strNew.splice(1, 1, 'ОБОЖАЕМ');
-str = strNew.join(' ');
-console.log(str);
+// let str = "Мы любим javascript";
+// const strNew = str.split(' ');
+// strNew.splice(1, 1, 'ОБОЖАЕМ');
+// str = strNew.join(' ');
+// console.log(str);
 
 
 // 2. Даны два массива
@@ -13,6 +13,19 @@ console.log(str);
 // let people2 = ['Connor', 'Stanley', 'Leo', 'Albert', 'Owen', 'Oliver', 'Ethan', 'Thomas'];
 
 // 1. Пользователь вводит имя, если это имя есть в массиве1 добавьте его копию в массив2
+
+// let people1 = ['Samuel', 'Jack', 'Thomas', 'Henry', 'Leo', 'Connor', 'David', 'Ryan'];
+// let people2 = ['Connor', 'Stanley', 'Leo', 'Albert', 'Owen', 'Oliver', 'Ethan', 'Thomas'];
+
+// let userName = prompt('Enter name');
+// for ( let i = 0; people1.length > i; i++) {
+//   if (userName === people1[i]) {
+//     people2.push(people1[i]);
+//   } else {
+//   }
+// }
+// console.log(people2);
+
 // 2. Пользователь вводит имя, если это имя есть в массиве2 удалите это имя из массива
 // 3. Создайте новый массив только из тех имен, которые есть в обоих массивах
 // 4. Объедините два массива так, чтобы в получившемся массиве не было одинаковых именgit
