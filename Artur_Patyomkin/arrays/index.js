1c3bf6ff6a035cd0de8348463abc6e92caa03802
Archi_Boom <artur.patyomkin@mail.ru>
2022-04-08 13:20:31 +0300

    task_3.2

--- Artur_Patyomkin/arrays/index.js
+++ Artur_Patyomkin/arrays/index.js
@@ -12,9 +12,26 @@
 
 // 3.1 С помощью цикла создать массив, каждый элемент которого равен квадрату своего номера.
 
-let arr = [];
+// let arr = [];
 
-for (let i = 0; arr.length !== 8; i++) {
-   arr[i] = i ** 2;
+// for (let i = 0; arr.length !== 8; i++) {
+//    arr[i] = i ** 2;
+// }
+// console.log(arr);
+
+
+// 3.2 С помощью цикла создать массив, который одинаково читается как слева направо, так и справа налево (Количество элементов массива: 9).
+
+let arr = [];
+arr.length = 9;
+let userNumber = 1;
+
+for (let i = 0, j = arr.length - 1; ; i++, j--, userNumber++) {
+   if (i === j) {
+      arr[i] = userNumber;
+      break;
+   }
+   arr[i] = userNumber;
+   arr[j] = arr[i];
 }
 console.log(arr);
