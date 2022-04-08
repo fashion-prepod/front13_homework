72b8a0e741168e4a15107bb415d29814bf80bae5
GorbunovaAnna <anyutachke@gmail.com>
2022-04-08 12:07:01 +0300

    added task 1

---
+++ Anna_Gorbunova/arrayMethods/index.js
@@ -0,0 +1,14 @@
+// 1. 
+// Пользователь вводит имя, если это имя есть в массиве1 добавьте его копию в массив2
+
+// let people1 = ['Samuel', 'Jack', 'Thomas', 'Henry', 'Leo', 'Connor', 'David', 'Ryan'];
+// let people2 = ['Connor', 'Stanley', 'Leo', 'Albert', 'Owen', 'Oliver', 'Ethan', 'Thomas'];
+
+// const userName = prompt('Enter name', '');
+
+// for (let i = 0; i < people1.length; i++) {
+//     if (userName === people1[i]) {
+//         people2.push(people1[i]);
+//     }
+// }
+// console.log(people2);
