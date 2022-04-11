8b9a2e1c72fbb21e83242cbb6e261816432f379a
Xaky <81919274+Xaky0105@users.noreply.github.com>
2022-04-11 23:36:47 +0300

    solved  task 1

---
+++ anton_starosotnikov/function/index.js
@@ -0,0 +1,27 @@
+// 1. Написать функцию, которая принимает число дня недели и возвращает
+// его название, если введено число больше 7 или меньше 1 вернуть
+// сообщение об ошибке. Пример работы функции: dayOfWeek(3) вернет
+// «Среда». Вызов dayOfWeek(8) вернет «Нет такого дня недели»
+
+// const userNum = prompt('enter a number from 1 to 7');
+// function dayOfWeek(userNum) {
+//     switch (userNum) {
+//         case '1': 
+//             return 'Monday'
+//         case '2': 
+//             return 'Tuesday'
+//         case '3': 
+//             return 'Wednesday'
+//         case '4': 
+//             return 'Thursday'
+//         case '5': 
+//             return 'Friday'
+//         case '6': 
+//             return 'Saturday'
+//         case '7': 
+//             return 'Sunday'
+//         default:
+//             return 'No such day of the week'  
+//     }
+// }
+// console.log(dayOfWeek(userNum));
