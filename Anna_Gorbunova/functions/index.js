f276e78d78af53d2e42db54339683c730e8e2e3e
GorbunovaAnna <anyutachke@gmail.com>
2022-04-14 13:54:49 +0300

    added task 1

---
+++ Anna_Gorbunova/functions/index.js
@@ -0,0 +1,30 @@
+// 1. 
+// Написать функцию, которая принимает число дня недели и возвращает
+// его название, если введено число больше 7 или меньше 1 вернуть
+// сообщение об ошибке. Пример работы функции: dayOfWeek(3) вернет
+// «Среда». Вызов dayOfWeek(8) вернет «Нет такого дня недели»
+
+// let userNumber = +prompt('Введите число дня недели');
+
+// function getDayOfWeek (userNumber) {
+//     if (userNumber < 1 || userNumber > 7) {
+//         return 'Нет такого дня недели';
+//     } else if (userNumber === 1) {
+//         return 'понедельник';
+//     } else if (userNumber === 2) {
+//         return 'вторник';
+//     } else if (userNumber === 3) {
+//         return 'среда';
+//     } else if (userNumber === 4) {
+//         return 'четверг';
+//     } else if (userNumber === 5) {
+//         return 'пятница';
+//     } else if (userNumber === 6) {
+//         return 'суббота';
+//     } else if (userNumber === 7) {
+//         return 'воскресенье';
+//     }
+// }
+
+// let dayOfWeek = getDayOfWeek(userNumber);
+// console.log(dayOfWeek);
