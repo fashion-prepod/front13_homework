6b444f6d1e6df885e003520c690a50eb7a24a576
Natallia <natkova86@mail.ru>
2022-04-15 17:37:39 +0300

    task 1 function index.js, index.html

---
+++ Nata_kovalevich/function/index.js
@@ -0,0 +1,30 @@
+'use strict'
+// 1. Написать функцию, которая принимает число дня недели и возвращает
+// его название, если введено число больше 7 или меньше 1 вернуть
+// сообщение об ошибке. Пример работы функции: dayOfWeek(3) вернет
+// «Среда». Вызов dayOfWeek(8) вернет «Нет такого дня недели»
+
+// let dayNumber = +prompt ('Enter the day of the week','');
+
+//  function dayOfWeek(dayNumber){
+
+//      if (  dayNumber < 1 || dayNumber > 7 ) {
+//          return 'Error.No such day of the week';
+//      } else if ( dayNumber === 1){
+//         return 'Monday';
+//      } else if ( dayNumber === 2){
+//         return 'Tuesday';
+//      } else if ( dayNumber === 3){
+//         return 'Wednesday';
+//      } else if ( dayNumber === 4){
+//         return 'Thursday';
+//      } else if ( dayNumber === 5){
+//         return 'Friday';
+//      } else if ( dayNumber === 6){
+//         return 'Saturday';
+//      } else if ( dayNumber === 7){
+//         return 'Sunday';
+//      }
+
+//  }
+//  console.log(dayOfWeek(dayNumber));
