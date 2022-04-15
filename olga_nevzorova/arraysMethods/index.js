3ff4efccc70b6afe31c9800dd756a865067151c7
neuzorovaolga <neuzorova@gmail.com>
2022-04-15 13:57:43 +0300

    arraysMethods task 2.2

--- olga_nevzorova/arraysMethods/index.js
+++ olga_nevzorova/arraysMethods/index.js
@@ -17,3 +17,14 @@ const userName = prompt();
 const index = people1.indexOf(userName);
 people2.push(people1[index]);
 console.log(people2);
+
+
+// 2.2. Пользователь вводит имя, если это имя есть в массиве2 удалите это имя из массива
+
+let people1 = ['Samuel', 'Jack', 'Thomas', 'Henry', 'Leo', 'Cannon', 'David', 'Ryan'];
+let people2 = ['Cannon', 'Stanly', 'Leo', 'Albert', 'Owen', 'Oliver', 'Ethan', 'Thomas'];
+
+const userName = prompt();
+const index = people2.indexOf(userName);
+people2.splice(index, 1);
+console.log(people2);
