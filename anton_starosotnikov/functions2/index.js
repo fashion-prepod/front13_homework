15bd30ac2c47270261ab1912747a3bda7916f09a
Xaky <81919274+Xaky0105@users.noreply.github.com>
2022-04-24 21:27:21 +0300

    solved task 1

---
+++ anton_starosotnikov/functions2/index.js
@@ -0,0 +1,15 @@
+// 1. Создать функцию «переключатель» toggleMe(),
+// которая при вызове чередует возвращаемые булевы
+// значения (true, false).
+
+// function toggler() {
+//     let bool = true;
+//     return function() {
+//         return bool = !bool;
+//     }
+// }
+
+// const toogleMe = toggler();
+// console.log(toogleMe());
+// console.log(toogleMe());
+// console.log(toogleMe());
