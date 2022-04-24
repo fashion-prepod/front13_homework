f94e5c98775cb473816f7b0635ccd5a3bbc1c450
Xaky <81919274+Xaky0105@users.noreply.github.com>
2022-04-24 21:55:46 +0300

    solved task 2

--- anton_starosotnikov/functions2/index.js
+++ anton_starosotnikov/functions2/index.js
@@ -13,3 +13,28 @@
 // console.log(toogleMe());
 // console.log(toogleMe());
 // console.log(toogleMe());
+
+
+// 2. Создать функцию funcCounter(), которая способна
+// считать количество своих вызовов. Если передать в эту
+// функцию число меньше 100, то функция считает
+// количество вызовов от этого числа до бесконечности,
+// если передать число больше 100, то функция считает
+// количество от этого числа до 0
+
+// function countCreator(callback) {
+//     let count = callback;
+//     return function counter() {
+//         if (callback < 100) {
+//             return ++count;
+//         } else {
+//             return count !== 0 ? --count : count;
+//         }
+//     } 
+// }
+
+// const funcCounter = countCreator(101);
+
+// console.log(funcCounter());
+// console.log(funcCounter());
+// console.log(funcCounter());
