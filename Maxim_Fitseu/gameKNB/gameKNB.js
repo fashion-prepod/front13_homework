5fe85dc27a7b614f6a88ab546d7d47e3cce41083
maxific <maxific@tut.by>
2022-04-28 15:46:33 +0300

    Game complete

--- Maxim_Fitseu/gameKNB/gameKNB.js
+++ Maxim_Fitseu/gameKNB/gameKNB.js
@@ -11,3 +11,67 @@
 // Math.round() отбросить дробную часть 
 // Math.round(Math.random() * 2)
 
+const arrAnswer = ['rock', 'scissors', 'paper'];
+function getUserChoice() {
+    for (;;) {
+        let userChoice = prompt('Select: rock, scissors, paper');
+        if (userChoice === null) {
+            return null;
+        }
+        if (isValidUserChoice(userChoice)) {
+            return userChoice;
+        } else {
+            console.log('Incorrect data');
+        }
+    }
+}
+
+function getCompChoice() {
+    const randomCompNumber = +Math.random().toFixed(2);
+    if (randomCompNumber < 0.33) {
+        return arrAnswer[0];
+    } else if (randomCompNumber >= 0.66) {
+        return arrAnswer[2];
+    } else {
+        return arrAnswer[1];
+    }
+}
+function isValidUserChoice(userChoice) {
+    return arrAnswer.includes(userChoice.toLowerCase());
+}
+function startGame() {
+    let numOfUserWins = 0
+    let numOfCompWins = 0
+    for (;;) {
+        const playerValue = getUserChoice();
+        const compValue = getCompChoice();
+        if (playerValue === null) {
+            return alert(`You left game`);
+        }
+        if (playerValue === compValue) {
+            alert(`Tie, ${numOfUserWins} : ${numOfCompWins}`);
+        } else if (
+            playerValue === arrAnswer[0] && compValue === arrAnswer[1] ||
+            playerValue === arrAnswer[1] && compValue === arrAnswer[2] ||
+            playerValue === arrAnswer[2] && compValue === arrAnswer[0]
+        ) {
+            alert(`Comp choice ${compValue}, you win, ${++numOfUserWins} : ${numOfCompWins}`);
+        } else {
+            alert(`Comp choice ${compValue}, you lose, ${numOfUserWins} : ${++numOfCompWins}`);
+        }
+        if (numOfUserWins === 10) {
+            alert(`You win with score: ${numOfUserWins} : ${numOfCompWins}`);
+            if (confirm('Another one?')) {
+                return startGame();
+            }
+            return;
+        } else if (numOfCompWins === 10) {
+            alert(`You lose with score ${numOfUserWins} : ${numOfCompWins}`);
+            if (confirm('Another one?')) {
+                return startGame();
+            }
+            return;
+        }
+    }
+}
+startGame();
