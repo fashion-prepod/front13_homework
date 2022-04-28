3973e166aa9b987ec78287303bb851cbe3eaf21a
Rybachenka <7381458@mail.ru>
2022-04-28 22:30:52 +0300

    added task 2

--- nataliya_rybachenko/callbacks_clousers/index.js
+++ nataliya_rybachenko/callbacks_clousers/index.js
@@ -2,21 +2,43 @@
 // которая при вызове чередует возвращаемые булевы
 // значения (true, false).
 
-function toggleMe() {
-  let value = true;
+// function toggleMe() {
+//   let value = true;
+//   return function () {
+//     if (value === true) {
+//       console.log(false);
+//       value = false;
+//     } else {
+//       console.log(true);
+//       value = true;
+//     }
+//   };
+// }
+// const result = toggleMe();
+
+// result();
+// result();
+// result();
+// result();
+
+// 2. Создать функцию funcCounter(), которая способна
+// считать количество своих вызовов. Если передать в эту
+// функцию число меньше 100, то функция считает
+// количество вызовов от этого числа до бесконечности,
+// если передать число больше 100, то функция считает
+// количество от этого числа до 0
+
+function funcCounter(number) {
+  let counter = number;
   return function () {
-    if (value === true) {
-      console.log(false);
-      value = false;
+    if (number < 100) {
+      return console.log(++counter);
     } else {
-      console.log(true);
-      value = true;
+      return console.log(--counter);
     }
   };
 }
-const result = toggleMe();
-
-result();
+const result = funcCounter(1002);
 result();
 result();
 result();
