de6c60ecba683a170eb136bb027028cba5f179cc
Sasha867 <sashacordoba@mail.ru>
2022-04-29 16:16:16 +0300

    Task_1

---
+++ sasha_lozichny/Arrays_methods_part_2/index.js
@@ -0,0 +1,12 @@
+// 1. вывести в консоль 'its valid' если во всех объектах a > b
+// вывести  'its invalid' если хотя бы в одном объекте a <= b
+
+// const arr = [
+//   { a: 2, b: 1 },
+//   { a: 5, b: 12 },
+//   { a: 95, b: 7 },
+//   { a: 51, b: 5 },
+// ];
+// const newArr = arr.every((object) => object.a > object.b);
+// console.log(newArr ? "its valid" : "its invalid");
+
