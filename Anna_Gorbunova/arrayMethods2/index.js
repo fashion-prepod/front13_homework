2dce25b53a60515ea423b121b6b20f2bba537d30
GorbunovaAnna <anyutachke@gmail.com>
2022-04-29 11:44:06 +0300

    added task 1.2

--- Anna_Gorbunova/arrayMethods2/index.js
+++ Anna_Gorbunova/arrayMethods2/index.js
@@ -14,3 +14,22 @@
 // })
 // console.log(res ? 'it\'s valid' : 'it\'s invalid');
 
+
+// 1.2 
+// на основе массива arr создать новый массив у объектов которого
+// поле b возведено в квадрат
+
+// const arr = [
+//     { a: 2, b: 1 },
+//     { a: 5, b: 12 },
+//     { a: 95, b: 7 },
+//     { a: 51, b: 5 },
+// ];
+
+// const newArr = arr.map((item) => {
+//     return {
+//         a: item.a,
+//         b: item.b ** 2
+//     };
+// })
+// console.log(newArr);
