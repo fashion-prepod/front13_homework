6a683e6f419c1882609888b4aa2fbc4988e9e3f3
Timofei Plaksitski <energye@tut.by>
2022-04-29 14:04:08 +0300

    Part2 arrays methods  task 4

--- PlaksitskiyT/arrays_methodsPart2/index.js
+++ PlaksitskiyT/arrays_methodsPart2/index.js
@@ -16,21 +16,29 @@
 // 2. Дан случайный массив чисел. С помощью метода .filter()
 // отфильтруйте массив так, чтобы в новый массив вошли
 // только четные двухзначные числа из исходного.
+
 // 3. Дан случайный массив имен. Выяснить есть ли в этом
 // массиве «Семён» с помощью метода .some().
+
 // 4. Дан массив случайных чисел. С помощью метода
 // .reduce() просуммировать только четные числа из этого
 // массива.
+
+let arr = [2, 1, 5, 12, 95, 7, 51, 5];
+let res = arr.reduce((acc, item) => item % 2 === 0 ? acc += item : acc, 0);
+console.log(res);
+
+
 // 5. Пользователь вводит 10 случайных значений. Каждое
 // значение необходимо записать в массив. С помощью
 // метода .every() проверить были ли все введенные
 // пользователем данные – числами.
 
-let arr = [];
-for ( let i = 0; arr.length < 10; i++){
-  let userNum = prompt('Enter number');
-  arr.push(userNum);
-};
- let tasks = arr.every((item) => !isNaN(item));
- console.log(arr);
- console.log(tasks);
+// let arr = [];
+// for ( let i = 0; arr.length < 10; i++){
+//   let userNum = prompt('Enter number');
+//   arr.push(userNum);
+// };
+//  let tasks = arr.every((item) => !isNaN(item));
+//  console.log(arr);
+//  console.log(tasks);
