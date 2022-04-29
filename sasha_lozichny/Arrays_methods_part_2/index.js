0191369c781c2b5b7b758e5eea6c351481f70a4f
Sasha867 <sashacordoba@mail.ru>
2022-04-29 16:17:42 +0300

    Task_1.2

--- sasha_lozichny/Arrays_methods_part_2/index.js
+++ sasha_lozichny/Arrays_methods_part_2/index.js
@@ -10,3 +10,17 @@
 // const newArr = arr.every((object) => object.a > object.b);
 // console.log(newArr ? "its valid" : "its invalid");
 
+// 1.2 на основе массива arr создать новый массив у объектов которого
+//    поле b возведено в квадрат
+
+// const arr = [
+//   { a: 2, b: 1 },
+//   { a: 5, b: 12 },
+//   { a: 95, b: 7 },
+//   { a: 51, b: 5 },
+// ];
+// const newArr = arr.map((item) => {
+//   return { a: item.a, b: item.b ** 2 };
+// });
+// console.log(newArr);
+
