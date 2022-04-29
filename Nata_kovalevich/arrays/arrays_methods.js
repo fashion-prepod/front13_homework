91318326780c44674cb81267abb491a41edb4d7e
Natallia <natkova86@mail.ru>
2022-04-29 16:16:31 +0300

    tasks arrays_methods.js

---
+++ Nata_kovalevich/arrays/arrays_methods.js
@@ -0,0 +1,99 @@
+// 1.    1.1 вывести в консоль 'its valid' если во всех оъектах a > b
+//    вывести  'its invalid' если хотя бы в одном объекте a <= b
+// const arr = [
+//     { a: 2, b: 1 },
+//     { a: 5, b: 12 },
+//     { a: 95, b: 7 },
+//     { a: 51, b: 5 },
+//   ];
+// let res = arr.every(function(item){ 
+//         return  item.a > item.b;
+// })
+// console.log(res ? 'its valid' : 'its invalid');
+
+
+// 1.2 на основе массива arr создать новый массив у объектов которого
+// поле b возведено в квадрат
+// const arr = [
+//     { a: 2, b: 1 },
+//     { a: 5, b: 12 },
+//     { a: 95, b: 7 },
+//     { a: 51, b: 5 },
+//   ];
+
+// let res = arr.reduce(function(acc, item){
+//   item.b = item.b**2;
+//   acc.push(item)
+//   return  acc ;
+// },[]) 
+// console.log(res);
+
+//    1.3 на основе массива arr созать массив где все значения полей
+//    a и b помещены как послеовательные значения
+//    [2, 1, 5, 12, 95, 7, 51, 5]
+// const arr = [
+//        { a: 2, b: 1 },
+//        { a: 5, b: 12 },
+//        { a: 95, b: 7 },
+//        { a: 51, b: 5 },
+//    ];
+
+// let res = arr.reduce(function(acc, item){
+//   acc.push(item.a, item.b)
+//   return  acc ;
+// },[]) 
+// console.log(res);
+  
+//   2. Дан случайный массив чисел. С помощью метода .filter()
+//   отфильтруйте массив так, чтобы в новый массив вошли
+//   только четные двухзначные числа из исходного.
+
+// const arr = [22, 33, 84, 95, 78 , 55, 1, 7];
+// let evenArr = arr.filter(function(even){
+//     return even % 2 === 0;
+// })
+// alert(evenArr);
+
+//   3. Дан случайный массив имен. Выяснить есть ли в этом
+//   массиве «Семён» с помощью метода .some().
+
+// const randomNames = ['Дима', 'Надя','Семен','Максим','Валера', 'Олеся', 'Влад','Витя'];
+// function semyon(name){
+//     return name === 'Семен';
+// } 
+// alert(randomNames.some(semyon));
+
+//   4. Дан массив случайных чисел. С помощью метода
+//   .reduce() просуммировать только четные числа из этого
+//   массива.
+
+// const arr = [123, 555, 888, 987, 562, 412, 1000, 4006];
+// let res = arr.reduce(function(sum, even){
+
+//         return sum += even % 2 ? 0 : even;
+
+// },0)
+// alert(res);
+
+//   5. Пользователь вводит 10 случайных значений. Каждое
+//   значение необходимо записать в массив. С помощью
+//   метода .every() проверить были ли все введенные
+//   пользователем данные – числами
+
+// const randomNum = [];
+
+// for(i = 0; i < 10; i++){   
+//    let user = prompt('Введите значение','') 
+//    if(user === null || user === ''){
+//        alert('Необходимо ввести значение','')
+//        break;
+//    } else {
+//         randomNum.push(user);
+//    } 
+// }
+// console.log(randomNum);
+
+// let res = randomNum.every(function(item){
+//     return !isNaN(item) ;
+// })
+// console.log(res);
--- Nata_kovalevich/arrays/index_methods.js
+++ Nata_kovalevich/arrays/index_methods.js
@@ -1,9 +1,9 @@
-// сформировать массив чисел, которые делятся на 3 (число элементов 8)
+// 1.сформировать массив чисел, которые делятся на 3 (число элементов 8)
 
 // const arr = [];
-// let numberArr = 0
+// let numberArr = 0;
 
-// for( i = 1; arr.length < 8; i++){
+// for( let i = 1; arr.length < 8; i++){
 //     if( i %3 === 0){
 //         arr[numberArr] = i;
 //         numberArr++;
@@ -11,26 +11,26 @@
 // }
 // console.log(arr);
 
-// Создать с помощью цикла массив, каждый элемент которого равен
+//2. Создать с помощью цикла массив, каждый элемент которого равен
 //  квадрату своего номера.
 
 // const arr = [];
-// let numberArr = 0
+// let numberArr = 0;
 
-// for( i = 1 ; arr.length < 10; i++){
+// for( let i = 1 ; arr.length < 10; i++){
     
-//         arr[numberArr] = i*i;
+//         arr[numberArr] = i * i;
 //         numberArr++;
     
 // }
 // console.log(arr);
 
-// Создать с помощью цикла массив, который одинаково читается как слева 
+// 3.Создать с помощью цикла массив, который одинаково читается как слева 
 // направо, так и спаво налево (9)
 
 // const arr = [];
 
-// for( i = 0 ; arr.length < 9; i++){
+// for( let i = 0 ; arr.length < 9; i++){
     
 //           if( i % 2 === 0){
 //                 arr[i] = 2;
@@ -40,13 +40,11 @@
 // }
 // console.log(arr);
 
-
-// Создать массив из 10 чисел.Вывести на экраны 
+// 4.Создать массив из 10 чисел.Вывести на экраны 
 // кол-во четных чисел из этого массива.
 
 // const arr = [ 2, 5, 54, 41, 98, 10, 63, 87, 88, 90, 101];
-// let evenNumber = 0;
-// for( i = 0 ; arr.length < 10 ; i++){
+// for(let i = 0 ; arr.length < 10 ; i++){
     
 //         if( arr[i] % 2 === 0){
 //             evenNumber++;        
@@ -58,15 +56,14 @@
 // наибольшее число из этого массива.
 
 // const arr = [2, 4, 8, 9, 14, 52, 7, 3, 22, 44, 10];
-
-// for (var i = 0; i < arr.length; i++) {
-//     if(arr[i] > arr[0]){
-//         arr[0] = arr[i];
+// let num = arr[i];
+// for (let i = 0; i < arr.length; i++) {
+//     if(arr[i] > num){
+//         num = arr[i];
 //     }
 
 // }
-// console.log(arr[0]);
-
+// console.log(num);
 
 // 1. Дана строка "Мы любим javascript"
 // изменить эту строку на "Мы ОБОЖАЕМ javascript"
@@ -79,10 +76,8 @@
 
 // 2. Даны два массива
 
-
 // 1. Пользователь вводит имя, если это имя есть в массиве1 
 // добавьте его копию в массив2
-
 
 // let people1 = ['Samuel', 'Jack', 'Thomas', 'Henry', 'Leo', 'Connor', 'David', 'Ryan'];
 // let people2 = ['Connor', 'Stanley', 'Leo', 'Albert', 'Owen', 'Oliver', 'Ethan', 'Thomas'];
@@ -105,15 +100,12 @@
 // let people1 = ['Samuel', 'Jack', 'Thomas', 'Henry', 'Leo', 'Connor', 'David', 'Ryan'];
 // let people2 = ['Connor', 'Stanley', 'Leo', 'Albert', 'Owen', 'Oliver', 'Ethan', 'Thomas'];
 
-
 // let userName = prompt('Please enter your name','');
 
 // for(i = 0; i < people2.lenght; i++){
 //     if(userName === people2[i]){
         
 //        people2.splice(i, 1);
-        
-
 //     }
 // }
 // console.log(people2);
@@ -123,7 +115,6 @@
 
 // let people1 = ['Samuel', 'Jack', 'Thomas', 'Henry', 'Leo', 'Connor', 'David', 'Ryan'];
 // let people2 = ['Connor', 'Stanley', 'Leo', 'Albert', 'Owen', 'Oliver', 'Ethan', 'Thomas'];
-
 
 // let newArr = [];
 // for (let i = 0; i < people1.length; i++) {
