96d870e4a055304f00bc44f55dd291fc466501e7
GorbunovaAnna <anyutachke@gmail.com>
2022-04-29 11:42:35 +0300

    added task 1.1

---
+++ Anna_Gorbunova/arrayMethods2/index.js
@@ -0,0 +1,16 @@
+// 1.1 
+// вывести в консоль 'its valid' если во всех объектах a > b
+// вывести  'its invalid' если хотя бы в одном объекте a <= b
+
+// const arr = [
+//     { a: 2, b: 1 },
+//     { a: 5, b: 12 },
+//     { a: 95, b: 7 },
+//     { a: 51, b: 5 },
+// ];
+
+// const res = arr.every((item) => {
+//     return item.a > item.b;
+// })
+// console.log(res ? 'it\'s valid' : 'it\'s invalid');
+
