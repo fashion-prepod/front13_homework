2fc944a3da0b080fb563fc1ce188306720773e2c
GorbunovaAnna <anyutachke@gmail.com>
2022-04-29 16:05:51 +0300

    added task 2

--- Anna_Gorbunova/callbaks_closures/index.js
+++ Anna_Gorbunova/callbaks_closures/index.js
@@ -15,3 +15,31 @@
 // console.log(result());
 // console.log(result());
 // console.log(result());
+
+
+// 2. 
+// Создать функцию funcCounter(), которая способна
+// считать количество своих вызовов. Если передать в эту
+// функцию число меньше 100, то функция считает
+// количество вызовов от этого числа до бесконечности,
+// если передать число больше 100, то функция считает
+// количество от этого числа до 0
+
+// function funcCounter(num) {
+//     let count = num;
+//     let action = num;
+//     return function() {
+//         if (action <= 100) {
+//             return count++;
+//         } else if (action > 100 && count >= 0) {
+//             return count--;
+//         }
+//         return 'The end';
+//     }
+
+// }
+// let counter = funcCounter(108);
+// console.log(counter());
+// console.log(counter());
+// console.log(counter());
+// console.log(counter());
