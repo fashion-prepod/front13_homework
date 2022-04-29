106f71d3412f172401d67a57dc3676610a5435f9
GorbunovaAnna <anyutachke@gmail.com>
2022-04-29 16:00:56 +0300

    added task 1

---
+++ Anna_Gorbunova/callbaks_closures/index.js
@@ -0,0 +1,17 @@
+// 1.
+// Создать функцию «переключатель» toggleMe(),
+// которая при вызове чередует возвращаемые булевы
+// значения (true, false).
+
+// function toggleMe () {
+//     let meeting = true;
+//     return function() {
+//         meeting = !meeting;
+//         return meeting;
+//     }
+// }
+// let result = toggleMe();
+// console.log(result());
+// console.log(result());
+// console.log(result());
+// console.log(result());
