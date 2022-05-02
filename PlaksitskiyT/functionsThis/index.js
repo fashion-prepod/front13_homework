155571f9d41f58ac3163e295242d62bef89055b2
Timofei Plaksitski <energye@tut.by>
2022-05-02 17:16:03 +0300

    HW functionsThis task 3

--- PlaksitskiyT/functionsThis/index.js
+++ PlaksitskiyT/functionsThis/index.js
@@ -1,29 +1,29 @@
-// 1. 
-const fbPlayers = [
-    {
-        name: 'Messi',
-        speed: 2
-    }, 
-    {
-        name: 'Ronaldo',
-        speed: 10
-    },
-    {
-        name: 'Zidane',
-        speed: 200
-    }
-];
+// // 1. 
+// const fbPlayers = [
+//     {
+//         name: 'Messi',
+//         speed: 2
+//     }, 
+//     {
+//         name: 'Ronaldo',
+//         speed: 10
+//     },
+//     {
+//         name: 'Zidane',
+//         speed: 200
+//     }
+// ];
 
-// написать функцию run которая выводит сообщение типа
-// "PLAYER_NAME runs with PLAYER_SPEEDkm/h"
-// "пробежать" по массиву fbPlayers и запустить run "от имени" каждого футболиста
+// // написать функцию run которая выводит сообщение типа
+// // "PLAYER_NAME runs with PLAYER_SPEEDkm/h"
+// // "пробежать" по массиву fbPlayers и запустить run "от имени" каждого футболиста
 
-function run () {
-    console.log(`${this.name} runs with ${this.speed} km/h`);
-};
-fbPlayers.forEach((players) => {
-    run.call(players);
-    });
+// function run () {
+//     console.log(`${this.name} runs with ${this.speed} km/h`);
+// };
+// fbPlayers.forEach((players) => {
+//     run.call(players);
+//     });
 
 
 // 2. "пробежаться" по массиву и добавить каждому футболисту метод .sayHello()
@@ -35,16 +35,19 @@ fbPlayers.forEach((players) => {
 // футболиста (используйте .forEach())
 
 
-// 3. const dog = {
-//     name: 'Bobik',
-//     bark: function () {
-//         console.log(`${this.name} barks`)
-//     }
-// };
+// 3. 
+const dog = {
+    name: 'Bobik',
+    bark: function () {
+        console.log(`${this.name} barks`)
+    }
+};
 
-// const person = {
-//     name: 'John',
-//     age: 20
-// };
+const person = {
+    name: 'John',
+    age: 20
+};
 
 // заставьте person сделать bark()
+
+dog.bark.apply(person)
