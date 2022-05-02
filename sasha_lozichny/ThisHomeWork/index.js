010f020d7d873be6fd9797c4c06b3f89e01f928f
Sasha867 <sashacordoba@mail.ru>
2022-05-02 17:06:37 +0300

    Task_1

---
+++ sasha_lozichny/ThisHomeWork/index.js
@@ -0,0 +1,27 @@
+// 1.написать функцию run которая выводит сообщение типа
+// "PLAYER_NAME runs with PLAYER_SPEEDkm/h"
+// "пробежать" по массиву fbPlayers и запустить run "от имени" каждого футболиста
+
+// const fbPlayers = [
+//   {
+//     name: "Messi",
+//     speed: 2,
+//   },
+//   {
+//     name: "Ronaldo",
+//     speed: 10,
+//   },
+//   {
+//     name: "Zidane",
+//     speed: 200,
+//   },
+// ];
+
+// function run() {
+//   console.log(`${this.name} runs with ${this.speed}km/h`);
+// }
+
+// fbPlayers.forEach((item) => {
+//   run.call(item);
+// });
+
