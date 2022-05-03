795f167e06f091b3db1b81c8fd05240938557f20
Archi_Boom <artur.patyomkin@mail.ru>
2022-05-03 21:57:01 +0300

    Task 1_3

--- Artur_Patyomkin/arrays/array_methods3.js
+++ Artur_Patyomkin/arrays/array_methods3.js
@@ -14,6 +14,32 @@
 // 1.2 на основе массива arr создать новый массив у объектов которого
 // поле b возведено в квадрат
 
+// const arr = [
+//    { a: 2, b: 1 },
+//    { a: 5, b: 12 },
+//    { a: 95, b: 7 },
+//    { a: 51, b: 5 },
+// ];
+
+// const newArr = arr.map((item) => {
+//    const newObject = {};
+//    for (const key in item) {
+//       if (key === 'b') {
+//          newObject[key] = item[key] ** 2;
+//       } else {
+//          newObject[key] = item[key];
+//       }
+//    }
+//    return newObject;
+// });
+// console.log(newArr);
+// console.log(arr);
+
+
+// 1.3 на основе массива arr созать массив где все значения полей
+// a и b помещены как послеовательные значения
+// [2, 1, 5, 12, 95, 7, 51, 5]
+
 const arr = [
    { a: 2, b: 1 },
    { a: 5, b: 12 },
@@ -21,16 +47,3 @@ const arr = [
    { a: 51, b: 5 },
 ];
 
-const newArr = arr.map((item) => {
-   const newObject = {};
-   for (const key in item) {
-      if (key === 'b') {
-         newObject[key] = item[key] ** 2;
-      } else {
-         newObject[key] = item[key];
-      }
-   }
-   return newObject;
-});
-console.log(newArr);
-console.log(arr);
