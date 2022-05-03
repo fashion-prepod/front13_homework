e5a7a20f9f74faf3f2b19778a6ba68fcb5911701
Archi_Boom <artur.patyomkin@mail.ru>
2022-05-03 21:54:28 +0300

    Task 1_2

--- Artur_Patyomkin/arrays/array_methods3.js
+++ Artur_Patyomkin/arrays/array_methods3.js
@@ -1,6 +1,19 @@
 // 1.1 вывести в консоль 'its valid' если во всех оъектах a > b
 // вывести  'its invalid' если хотя бы в одном объекте a <= b
 
+// const arr = [
+//    { a: 2, b: 1 },
+//    { a: 5, b: 12 },
+//    { a: 95, b: 7 },
+//    { a: 51, b: 5 },
+// ];
+
+// const result = arr.every((item) => item.a > item.b);
+// console.log(result ? 'its valid' : 'its invalid');
+
+// 1.2 на основе массива arr создать новый массив у объектов которого
+// поле b возведено в квадрат
+
 const arr = [
    { a: 2, b: 1 },
    { a: 5, b: 12 },
@@ -8,5 +21,16 @@ const arr = [
    { a: 51, b: 5 },
 ];
 
-const result = arr.every((item) => item.a > item.b);
-console.log(result ? 'its valid' : 'its invalid');
+const newArr = arr.map((item) => {
+   const newObject = {};
+   for (const key in item) {
+      if (key === 'b') {
+         newObject[key] = item[key] ** 2;
+      } else {
+         newObject[key] = item[key];
+      }
+   }
+   return newObject;
+});
+console.log(newArr);
+console.log(arr);
