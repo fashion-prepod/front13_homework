8b02361d3b23cf7d3001c776e50dddf5bcb054a9
neuzorovaolga <neuzorova@gmail.com>
2022-05-04 12:19:01 +0300

    task 2

--- olga_nevzorova/functions2/index.js
+++ olga_nevzorova/functions2/index.js
@@ -13,3 +13,29 @@ const result = toogleMe();
 console.log(result());
 console.log(result());
 console.log(result());
+
+
+// 2. Создать функцию funcCounter(), которая способна
+// считать количество своих вызовов. Если передать в эту
+// функцию число меньше 100, то функция считает
+// количество вызовов от этого числа до бесконечности,
+// если передать число больше 100, то функция считает
+// количество от этого числа до 0
+
+function countCreator(number) {
+    if (number < 100) {
+        let counter = number;
+        return function count() {
+            return counter++;
+        }
+    } else {
+        let counter = number;
+        return function count() {
+            return counter--;
+        }
+    }
+}
+const result = countCreator(9);
+console.log(result());
+console.log(result());
+console.log(result());
