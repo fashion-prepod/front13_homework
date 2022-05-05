fdcdf2da268033d8a4663a5c52656d42f5c00786
Sasha867 <sashacordoba@mail.ru>
2022-05-05 21:56:02 +0300

    Task_2

--- sasha_lozichny/callbacksHomeWork/index.js
+++ sasha_lozichny/callbacksHomeWork/index.js
@@ -13,3 +13,27 @@
 // console.log(toggleMe());
 // console.log(toggleMe());
 
+// 2. Создать функцию funcCounter(), которая способна
+// считать количество своих вызовов. Если передать в эту
+// функцию число меньше 100, то функция считает
+// количество вызовов от этого числа до бесконечности,
+// если передать число больше 100, то функция считает
+// количество от этого числа до 0
+
+// function funcCounter(number) {
+//   let countValue = number;
+//   return function () {
+//     if (number < 100) {
+//       return ++countValue;
+//     } else if (number > 99) {
+//       return --countValue;
+//     }
+//   };
+// }
+// const counterFunc = funcCounter(100);
+// console.log(counterFunc());
+// console.log(counterFunc());
+// console.log(counterFunc());
+
+
+
