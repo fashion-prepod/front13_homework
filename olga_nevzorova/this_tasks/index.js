d4f600e802d8ba4c253a3ab762e2099ae973ced8
neuzorovaolga <neuzorova@gmail.com>
2022-05-06 00:42:48 +0300

    task 1

---
+++ olga_nevzorova/this_tasks/index.js
@@ -0,0 +1,25 @@
+// 1.
+const fbPlayers = [
+    {
+        name: 'Messi',
+        speed: 2
+    },
+    {
+        name: 'Ronaldo',
+        speed: 10
+    },
+    {
+        name: 'Zidane',
+        speed: 200
+    }
+];
+
+// написать функцию run которая выводит сообщение типа
+// "PLAYER_NAME runs with PLAYER_SPEEDkm/h"
+// "пробежать" по массиву fbPlayers и запустить run "от имени" каждого футболиста
+
+function run() {
+    console.log(`${this.name} runs with ${this.speed} km/h`)
+}
+
+fbPlayers.forEach((item) => { run.call(item) })
