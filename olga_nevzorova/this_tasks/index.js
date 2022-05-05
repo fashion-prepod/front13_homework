19205d9484a9d328e62a5276f8e985e1edaa1134
neuzorovaolga <neuzorova@gmail.com>
2022-05-06 00:43:36 +0300

    task 2

--- olga_nevzorova/this_tasks/index.js
+++ olga_nevzorova/this_tasks/index.js
@@ -23,3 +23,27 @@ function run() {
 }
 
 fbPlayers.forEach((item) => { run.call(item) })
+
+
+// 2. "пробежаться" по массиву и добавить каждому футболисту метод .sayHello()
+// который выводит сообщение вида "Hello! My name is PLAYER_NAME"
+// создайте функцию presentation(callback), которая выводит сообщение вида
+// "Ladies and Gentleman here is..." после чего запускает callback
+
+// в качестве коллбэка передайте метод .sayHello() каждого 
+// футболиста (используйте .forEach())
+
+
+
+function sayHello() {
+    console.log(`Hello! My name is ${this.name}`)
+}
+
+function presentation(callback) {
+    console.log(`Ladies and Gentleman here is...`)
+    callback()
+}
+
+fbPlayers.forEach((item) => {
+    presentation(item.sayHello = sayHello.bind(item));
+})
