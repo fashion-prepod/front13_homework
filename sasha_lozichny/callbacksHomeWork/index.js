afccdc62eac0a854b2e17ef73a180fa4820466ac
Sasha867 <sashacordoba@mail.ru>
2022-05-05 21:53:47 +0300

    Task_1

---
+++ sasha_lozichny/callbacksHomeWork/index.js
@@ -0,0 +1,15 @@
+// // 1. Создать функцию «переключатель» toggleMe(),
+// // которая при вызове чередует возвращаемые булевы
+// // значения (true, false).
+
+// function showBooleanValue() {
+//   let valueBoolean = false;
+//   return () => {
+//     valueBoolean = !valueBoolean;
+//     return valueBoolean;
+//   };
+// }
+// const toggleMe = showBooleanValue();
+// console.log(toggleMe());
+// console.log(toggleMe());
+
