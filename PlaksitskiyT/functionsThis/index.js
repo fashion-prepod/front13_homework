7473b6721dcd0305e9fb37b313d5cf7452cf4448
Timofei Plaksitski <energye@tut.by>
2022-05-15 16:01:15 +0300

    Nechto

--- PlaksitskiyT/functionsThis/index.js
+++ PlaksitskiyT/functionsThis/index.js
@@ -1,30 +1,18 @@
-// // 1. 
-// const fbPlayers = [
-//     {
-//         name: 'Messi',
-//         speed: 2
-//     }, 
-//     {
-//         name: 'Ronaldo',
-//         speed: 10
-//     },
-//     {
-//         name: 'Zidane',
-//         speed: 200
-//     }
-// ];
 
-// // написать функцию run которая выводит сообщение типа
-// // "PLAYER_NAME runs with PLAYER_SPEEDkm/h"
-// // "пробежать" по массиву fbPlayers и запустить run "от имени" каждого футболиста
-
-// function run () {
-//     console.log(`${this.name} runs with ${this.speed} km/h`);
-// };
-// fbPlayers.forEach((players) => {
-//     run.call(players);
-//     });
-
+const fbPlayers = [
+    {
+        name: 'Messi',
+        speed: 2
+    },
+    {
+        name: 'Ronaldo',
+        speed: 10
+    },
+    {
+        name: 'Zidane',
+        speed: 200
+    }
+];
 
 // 2. "пробежаться" по массиву и добавить каждому футболисту метод .sayHello()
 // который выводит сообщение вида "Hello! My name is PLAYER_NAME"
@@ -34,20 +22,21 @@
 // в качестве коллбэка передайте метод .sayHello() каждого 
 // футболиста (используйте .forEach())
 
-
-// 3. 
-const dog = {
-    name: 'Bobik',
-    bark: function () {
-        console.log(`${this.name} barks`)
-    }
+function sayHello() {
+    console.log(`Hello! My name is ${this.name}`);
 };
 
-const person = {
-    name: 'John',
-    age: 20
-};
+function presentation(callback) {
+        console.log(`Ladies and Gentleman here is...`);
+        callback()
+    };
+fbPlayers.forEach((item) => {
+    presentation(item.sayHello = sayHello.bind(item));
+});
 
-// заставьте person сделать bark()
 
-dog.bark.apply(person)
+
+
+// fbPlayers.forEach((item) => {
+//     presentation(item.sayHello = sayHello.bind(item));
+// })
