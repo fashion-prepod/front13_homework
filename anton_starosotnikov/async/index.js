adb160b9a0693b19e3e6f2ef9b176792725ca7c5
Xaky <81919274+Xaky0105@users.noreply.github.com>
2022-06-02 20:45:44 +0300

    refactoring

--- anton_starosotnikov/async/index.js
+++ anton_starosotnikov/async/index.js
@@ -1,6 +1,8 @@
-const getDataOfUsers = async() => {
+const API_URL_USERS = 'https://reqres.in/api/users?page=1';
+
+const getDataOfUsers = async(url) => {
     try {
-        const response = await fetch('https://reqres.in/api/users?page=1');
+        const response = await fetch(url);
         return await response.json();
     } catch (err) {
         document.write(`<h1>${err.message}, try again</h1>`);
@@ -17,7 +19,7 @@ const isUserInArrNames = (arrNames) => {
 }
 
 const showListOfNames = async() => {
-    const data = await getDataOfUsers();
+    const data = await getDataOfUsers(API_URL_USERS);
     const users = data.data;
     const usersNames = users.map(user => user.first_name);
     usersNames.forEach((name, index) => {
