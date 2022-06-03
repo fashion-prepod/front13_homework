3b97066ee9263fba72c1384b81a827e44527647b
Xaky <81919274+Xaky0105@users.noreply.github.com>
2022-06-03 22:27:55 +0300

    refactoring 2

--- anton_starosotnikov/async/index.js
+++ anton_starosotnikov/async/index.js
@@ -12,23 +12,34 @@ const getDataOfUsers = async(url) => {
 
 const isUserInArrNames = (arrNames) => {
     const userName = prompt('Enter username');
-    document.write(arrNames.includes(userName) ? `
-        <p><b>${userName}</b> found in the list</p>` : 
-        `<p><b>${userName}</b> NOT found in the list</p>
-    `);
+    document.write(arrNames.includes(userName) ? 
+        `<p><b>${userName}</b> found in the list</p>` : 
+        `<p><b>${userName}</b> NOT found in the list</p>`
+    );
 }
 
 const showListOfNames = async() => {
     const data = await getDataOfUsers(API_URL_USERS);
     const users = data.data;
-    const usersNames = users.map(user => user.first_name);
-    usersNames.forEach((name, index) => {
+    // const usersNames = users.map(user => user.first_name);
+    // usersNames.forEach((name, index) => {
+    //     document.write(`
+    //         ${index === 0 ? '<ul>' : ''}
+    //             <li>${name}</li>
+    //         ${index === usersNames.length - 1 ? '</ul>' : ''}
+    //     `);
+    // });
+    // Понял, что закомменченый код выше мне показался не очень хорошим, т.к там использовал методы и map и forEach. А эти методы имеют не самую быструю скорость работы O(n),
+    // Поэтому решил переписать только с методом map, т.к это ускорит работу, особенно если бы нужно было обрабатывать много данных.
+    const usersNames = users.map((user, index) => {
         document.write(`
             ${index === 0 ? '<ul>' : ''}
-                <li>${name}</li>
-            ${index === usersNames.length - 1 ? '</ul>' : ''}
+                 <li>${user.first_name}</li>
+            ${index === users.length - 1 ? '</ul>' : ''}
         `);
-    });
+        return user.first_name;
+    })
+
     setTimeout(() => {
         isUserInArrNames(usersNames);
     }, 1000);
